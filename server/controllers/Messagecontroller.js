32f48d47cee8346bc0442c4793457703f25e80f3
GeekPrathamesh <prathameshteli727@gmail.com>
2025-12-30 18:16:42 +0530

    added socket.io live interaction chat

--- server/controllers/Messagecontroller.js
+++ server/controllers/Messagecontroller.js
@@ -1,5 +1,7 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/MessageModel.js";
 import User from "../models/Usermodel.js";
+import { io, userSocketMap } from "../server.js";
 
 //get the all users
 export const getUsers = async (req, res) => {
@@ -11,50 +13,89 @@ export const getUsers = async (req, res) => {
     // count messages that not seen
     const unseenMessages = {};
     const promises = filteredUsers.map(async (user) => {
-      const messages = await Message.find({senderId:user._id,receiverId:userId,seen:false});
-      if(messages.length>0){
-        unseenMessages[user._id]=messages.length;
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
       }
     });
     await Promise.all(promises);
-    res.json({success:true,users:filteredUsers,unseenMessages})
+    res.json({ success: true, users: filteredUsers, unseenMessages });
   } catch (error) {
     console.log(error.message);
 
-        res.json({success:false,message:error.message})
-
+    res.json({ success: false, message: error.message });
   }
 };
 // get all messages of selected user
-export const getMessagesselecteduser=async(req,res)=>{
-    try {
-        const {id:selectedId}=req.params;
-        const myId = req.user._id;
-        const messages = await Message.find({
-            $or:[{sendorId:myId,receiverId:selectedId},
-                {sendorId:selectedId,receiverId:myId},
+export const getMessagesselecteduser = async (req, res) => {
+  try {
+    const { id: selectedId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { sendorId: myId, receiverId: selectedId },
+        { sendorId: selectedId, receiverId: myId },
+      ],
+    });
 
-            ]
-        })
+    await Message.updateMany(
+      { sendorId: selectedId, receiverId: myId },
+      { seen: true }
+    );
+    res.json({ success: true, messages });
+  } catch (error) {
+    console.log(error.message);
 
-        await Message.updateMany({sendorId:selectedId,receiverId:myId},{seen:true});
-        res.json({success:true,messages})
-    } catch (error) {
-            console.log(error.message);
-
-        res.json({success:false,message:error.message})
-    }
-}
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // api to make message as seen while chatting
-export const markMessageseen=async(req,res)=>{
-    try {
-        const {id}=req.params;
-        await Message.findByIdAndUpdate(id,{seen:true});
-        res.json({success:true,})
-    } catch (error) {
-                    console.log(error.message);
+export const markMessageseen = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Message.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
 
-        res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// send message to selected user
+
+export const sendMessage = async (req, res) => {
+  try {
+    const {text,image}=req.body;
+    const receiverId = req.params.id;
+    const sendorId=req.user._id;
+
+    let imageUrl ;
+    if(image){
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl=uploadResponse.secure_url;
+
     }
-}
+    const newMessage = await Message.create({
+      senderId,receiverId,text,image:imageUrl
+    });
+
+    //emit new message to receiver socket
+    const receiverSocketId= userSocketMap[receiverId];
+    if(receiverId){
+      io.to(receiverSocketId).emit("newMessage",newMessage)
+    }
+
+    
+    res.json({success:true,newMessage})
+  } catch (error) {
+    console.log(error.message);
+
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protectedRoute } from "../middleware/Auth.js";
-import {  getMessagesselecteduser, getUsers, markMessageseen } from "../controllers/Messagecontroller.js";
+import {  getMessagesselecteduser, getUsers, markMessageseen, sendMessage } from "../controllers/Messagecontroller.js";
 const messageRouter = express.Router();
 
 messageRouter.get("/users",protectedRoute,getUsers);
 messageRouter.get("/:id",protectedRoute,getMessagesselecteduser);
-messageRouter.put("mark/:id",protectedRoute,markMessageseen);
+messageRouter.put("/mark/:id",protectedRoute,markMessageseen);
+messageRouter.post("/send/:id",protectedRoute,sendMessage);
+
 export default messageRouter;
